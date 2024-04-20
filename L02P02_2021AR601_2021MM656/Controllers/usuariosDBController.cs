aac10d0bdaa1ab8cf235f5d5cae5cf01305b74c3
normix2003 <arrivillagariveran@gmail.com>
2024-04-20 11:19:49 -0600

    agrege algo en controller

--- L02P02_2021AR601_2021MM656/Controllers/usuariosDBController.cs
+++ L02P02_2021AR601_2021MM656/Controllers/usuariosDBController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using L02P02_2021AR601_2021MM656.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace L02P02_2021AR601_2021MM656.Controllers
 {
+
+
     public class usuariosDBController : Controller
     {
+        private readonly UsuariosDbContext _UsuariosDbContext;
+
+        public usuariosDBController(UsuariosDbContext UsuariosDbContext)
+        {
+            _UsuariosDbContext = UsuariosDbContext;
+        }
         public IActionResult Index()
         {
             return View();
