bf6771515c56de4a1b76c7a221d6eb2745d4620f
Carlos Alejandro Murga Martínez <carlos.murga1@catolica.edu.sv>
2024-04-20 10:17:12 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2021AR601_2021MM656/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2021AR601_2021MM656.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
