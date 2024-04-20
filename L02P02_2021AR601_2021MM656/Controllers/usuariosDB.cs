de9d3a4819ae91c154d4e99032df1d55d4dc8cc9
normix2003 <arrivillagariveran@gmail.com>
2024-04-20 10:52:53 -0600

    controlador y vista

---
+++ L02P02_2021AR601_2021MM656/Controllers/usuariosDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2021AR601_2021MM656.Controllers
+{
+    public class usuariosDB : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
